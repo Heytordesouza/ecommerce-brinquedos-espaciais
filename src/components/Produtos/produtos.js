a60012314edb8b84a56431f6ea01e2c3c8b5e2f7
Heytordesouza <heytordesnascimento@gmail.com>
2023-08-15 18:17:28 -0300

    Adicionado processador de pagamentos

--- src/components/Produtos/produtos.js
+++ src/components/Produtos/produtos.js
@@ -8,8 +8,13 @@ import imgId7 from "../../../public/img/id7.png"
 import imgId8 from "../../../public/img/id8.png"
 import imgId9 from "../../../public/img/id9.png"
 import imgId10 from "../../../public/img/id10.png"
+import imgId11 from "../../../public/img/id11.png"
+import imgId12 from "../../../public/img/id12.png"
+import imgId13 from "../../../public/img/id13.png"
+import imgId14 from "../../../public/img/id14.png"
+import imgId15 from "../../../public/img/id15.png"
 
-const Produtos =  [
+const Produtos = [
   {
     id: 1,
     nome: "Ônibus e Rover Espaciais com 2 Astronautas",
@@ -39,37 +44,67 @@ const Produtos =  [
     nome: "Pistola Missão Espacial com Projetor + Som",
     valor: 49.99,
     imagemUrl: imgId5,
- },
- {
+  },
+  {
     id: 6,
     nome: "Kit de Construção Espacial, 13 em 1",
     valor: 329.99,
     imagemUrl: imgId6,
- },
- {
+  },
+  {
     id: 7,
     nome: "Réplica Ônibus Espacial Shuttle Nasa Columbia",
     valor: 399.99,
     imagemUrl: imgId7,
- },
- {
+  },
+  {
     id: 8,
     nome: "Star Wars Millenium Falcon com Miniaturas",
     valor: 649.99,
     imagemUrl: imgId8,
- },
- {
+  },
+  {
     id: 9,
     nome: "Lego Marvel Nave Dos Guardiões Da Galáxia",
     valor: 1299.99,
     imagemUrl: imgId9,
- },
- {
-  id: 10,
-  nome: "Kit Sensorial - ASTRONAUTAS",
-  valor: 164.99,
-  imagemUrl: imgId10,
-}
+  },
+  {
+    id: 10,
+    nome: "Kit Sensorial - ASTRONAUTAS",
+    valor: 164.99,
+    imagemUrl: imgId10,
+  },
+  {
+    id: 11,
+    nome: "Nave Espacial Buzz Lightyear",
+    valor: 279.99,
+    imagemUrl: imgId11,
+  },
+  {
+    id: 12,
+    nome: "Caminhão Espacial Didático 249 Peças",
+    valor: 114.99,
+    imagemUrl: imgId12,
+  },
+  {
+    id: 13,
+    nome: "Kit Montar Robô Solar 6 em 1 c/ Estação Espacial ",
+    valor: 119.99,
+    imagemUrl: imgId13,
+  },
+  {
+    id: 14,
+    nome: "Kit Estação Solar Robótica Robô Didático 3 Em 1",
+    valor: 109.99,
+    imagemUrl: imgId14,
+  },
+  {
+    id: 15,
+    nome: "Foguete Espacial De Astronauta Com Luz e Som",
+    valor: 299.99,
+    imagemUrl: imgId15,
+  }
 ]
 
 export default Produtos;
